ee9b371ec122d46c8675f5a752a7727c0197a6e1
bjcmf <bernardojcm.filho@gmail.com>
2024-05-10 14:35:21 -0300

    cria funcao gerar

--- index.js
+++ index.js
@@ -5,12 +5,11 @@ const classe = document.getElementById("classe");
 const mensage = document.getElementById("mensage");
 const check1 = document.getElementById("check1");
 const cidadesId = document.getElementById("cidadesId");
-const listaIdaEvolta = cidades.idaEvolta;
-const listaIda = cidades.ida;
+
 const cidades = { 
     'idaEvolta': [
         'Sao paulo',
-        'Rio de janeiro', 
+        'Rio de janeiro',
         'Salvador',
         'Curitiba',
         'Recife',
@@ -25,43 +24,42 @@ const cidades = {
 
 function teste(){
     const checkbox = check1.checked
-    const element = cidades.idaEvolta;
-    if (checkbox == true) {
-        for (let i = 0; i < element.length - 1; i++) {
-            if (origem.value == element[i] && destino.value == element[i + 1]) {
-                console.log('passou');
-            } else {
-                console.log('nao passou');
-            }
-        }
-    }
     const array = [origem, destino, passagens, classe];
     for (let index = 0; index < array.length; index++) {
         if (array[index].value == 0){
             mensage.innerHTML = 'Preencha todos os campos'
         } else {
             mensage.innerHTML = ''
+            if (checkbox == true) {
+                for (let i = 0; i < cidades['idaEvolta'].length - 1; i++) {
+                    const element = cidades['idaEvolta'];
+                    if (origem.value == element[i] && destino.value == element[i + 1]) {
+                        alert('passou');
+                        break
+                    } else {
+                        console.log('nao passou');
+                    }
+                }
+            }
         }
     }
 }
 
+const listaIdaEvolta = cidades.idaEvolta;
+const listaIda = cidades.ida;
 
-
-
-for (let i = 0; i < listaIdaEvolta.length - 1; i++) {
-    const cidade = document.createElement("p");
-    const text = document.createTextNode(`${listaIdaEvolta[i]} -> ${listaIdaEvolta[i + 1]}, possui ida e volta`)
-    cidade.appendChild(text);
-    cidadesId.appendChild(cidade);
+const gerar = (param) => {
+    for (let i = 0; i < param.length - 1; i++) {
+        const cidade = document.createElement("p");
+        const text = document.createTextNode(`${param[i]} -> ${param[i + 1]}, possui ida e volta`)
+        cidade.appendChild(text);
+        cidadesId.appendChild(cidade);
+    }
 }
 
+gerar(listaIdaEvolta)
+gerar(listaIda)
 
-for (let i = 0; i < listaIda.length - 1; i++) {
-    const cidade = document.createElement("p");
-    const text = document.createTextNode(`${listaIda[i]} -> ${listaIda[i + 1]}, não possui ida e volta`)
-    cidade.appendChild(text);
-    cidadesId.appendChild(cidade);
-}
 
 function renderPage() {
     console.log('click');
