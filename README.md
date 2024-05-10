1207cd4a7d0c9fe1ccce5a697cbc09ee8c23bb58
BernardoJFilho <bernardojcm.filho@gmail.com>
2024-05-10 13:45:09 -0300

    renderiza padina cidades

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # trainneHTML
-Primeiro projeto trainee
+
+Este projeto tem como objetivo diminuir a complexidade de sistemas de reserva de passagens , proporcionando uma plataforma acessível e amigável para a compra de passagens de trem. A criação dessa interface oferece uma experiência simplificada  aos usuários que desejam adquirir passagens de trem online. Utilizando HTML e CSS, a interface foi desenvolvida conforme o design estabelecido, e através deste projeto, adquiri habilidades em HTML, CSS e design de interface, além de experiência prática em desenvolvimento web.
 
 link -> https://bernardojfilho.github.io/trainneHTML/
--- index.js
+++ index.js
@@ -5,36 +5,8 @@ const classe = document.getElementById("classe");
 const mensage = document.getElementById("mensage");
 const check1 = document.getElementById("check1");
 const cidadesId = document.getElementById("cidadesId");
-
-function teste(){
-    const checkbox = check1.checked
-    const array = [origem, destino, passagens, classe];
-    for (let index = 0; index < array.length; index++) {
-        if (array[index].value == 0){
-            mensage.innerHTML = 'Preencha todos os campos'
-        } else {
-            mensage.innerHTML = ''
-        }
-    }
-    // cidades['ida'].map(element => console.log(element))
-}
-
-function renderPage() {
-    
-}
-cidades['idaEvolta'].forEach(element => {
-    const cidade = document.createElement("p");
-    const text = document.createTextNode(element)
-    cidade.appendChild(text);
-    cidadesId.appendChild(cidade);
-});
-// const cidade = document.createElement("p");
-// const text = document.createTextNode("salve")
-// cidade.appendChild(text);
-// cidadesId.appendChild(cidade);
-
-
-
+const listaIdaEvolta = cidades.idaEvolta;
+const listaIda = cidades.ida;
 const cidades = { 
     'idaEvolta': [
         'Sao paulo',
@@ -50,3 +22,48 @@ const cidades = {
         'Manaus',
     ]
 }
+
+function teste(){
+    const checkbox = check1.checked
+    const element = cidades.idaEvolta;
+    if (checkbox == true) {
+        for (let i = 0; i < element.length - 1; i++) {
+            if (origem.value == element[i] && destino.value == element[i + 1]) {
+                console.log('passou');
+            } else {
+                console.log('nao passou');
+            }
+        }
+    }
+    const array = [origem, destino, passagens, classe];
+    for (let index = 0; index < array.length; index++) {
+        if (array[index].value == 0){
+            mensage.innerHTML = 'Preencha todos os campos'
+        } else {
+            mensage.innerHTML = ''
+        }
+    }
+}
+
+
+
+
+for (let i = 0; i < listaIdaEvolta.length - 1; i++) {
+    const cidade = document.createElement("p");
+    const text = document.createTextNode(`${listaIdaEvolta[i]} -> ${listaIdaEvolta[i + 1]}, possui ida e volta`)
+    cidade.appendChild(text);
+    cidadesId.appendChild(cidade);
+}
+
+
+for (let i = 0; i < listaIda.length - 1; i++) {
+    const cidade = document.createElement("p");
+    const text = document.createTextNode(`${listaIda[i]} -> ${listaIda[i + 1]}, não possui ida e volta`)
+    cidade.appendChild(text);
+    cidadesId.appendChild(cidade);
+}
+
+function renderPage() {
+    console.log('click');
+}
+
