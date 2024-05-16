75ed11ebfdf8c85765f1e665e254d3f893dacd30
BernardoJFilho <bernardojcm.filho@gmail.com>
2024-05-16 11:32:38 -0300

    refatora funcoes, implementa css

--- index.js
+++ index.js
@@ -26,26 +26,26 @@ function teste(){
     const checkbox = check1.checked
     const array = [origem, destino, passagens, classe];
     for (let index = 0; index < array.length; index++) {
-        if (array[index].value == 0){
+        if (array[index].value === 0){
             mensage.innerHTML = 'Preencha todos os campos'
         } else {
             mensage.innerHTML = ''
-            if (checkbox == true) {
-                if (funcCheckbox(origem, destino) == false){
-                    const mensage = 'Nao tem passagens de ida e volta para esses lugares';
-                    alert(mensage)
+            if (checkbox) {
+                if (!funcCheckbox(origem, destino)){
+                    const mensagem = 'Nao tem passagens de ida e volta para esses lugares';
+                    alert(mensagem)
                     check1.checked = false;
                     break;
                 }
                 else {
-                    const mensage = 'Buscando dados';
-                    alert(mensage);
+                    const mensagem = 'Buscando dados';
+                    alert(mensagem);
                     break;
                 }
             }
             else {
-                const mensage = 'Buscando dados';
-                alert(mensage);
+                const mensagem = 'Buscando dados';
+                alert(mensagem);
                 break;
             }
         }
@@ -53,10 +53,14 @@ function teste(){
 }
 
 const funcCheckbox = (param1, param2) => {
-    for (let i = 0; i < cidades['idaEvolta'].length - 1; i++) {
-        const element = cidades['idaEvolta'];
-        if (param1.value == element[i] && param2.value == element[i + 1]) {
-            return true;
+    const elements = cidades['idaEvolta'];
+    for (let i = 0; i < elements.length - 1; i++) {
+        const city1 = elements[i];
+        for (let j = 0; j < elements.length; j++) {
+            const city2 = elements[j];
+            if (param1.value === city1 && param2.value === city2 && city1 !== city2) {
+                return true;
+            }
         }
     }
     return false;
@@ -65,25 +69,29 @@ const funcCheckbox = (param1, param2) => {
 const listaIdaEvolta = cidades.idaEvolta;
 const listaIda = cidades.ida;
 
+const criarParagrafo = (city1, city2, idaOuVolta) => {
+    const cidade = document.createElement("p");
+    const texto = document.createTextNode(`${city1} -> ${city2}, possui ${idaOuVolta ? 'ida e volta' : 'passagem só de ida'}`);
+    cidade.appendChild(texto);
+    cidade.style.color = idaOuVolta ? 'green' : 'red';
+    cidadesId.appendChild(cidade);
+}
+
 const gerar = (param) => {
+    const isSaoPaulo = param[0] === 'Sao paulo';
     for (let i = 0; i < param.length - 1; i++) {
-        const cidade = document.createElement("p");
-        let text = ''
-        if (param.length == 4) {
-            text = document.createTextNode(`${param[i]} -> ${param[i + 1]}, possui passagem so de ida`)
-        } else {
-            text = document.createTextNode(`${param[i]} -> ${param[i + 1]}, possui ida e volta`)
+        const element = param[i];
+        for (let j = 0; j < param.length; j++) {
+            const element2 = param[j];
+            if (element !== element2 && isSaoPaulo) {
+                criarParagrafo(element, element2, true);
+            }
+            if (element !== element2 && !isSaoPaulo) {
+                criarParagrafo(element, element2, false)
+            }
         }
-        cidade.appendChild(text);
-        cidadesId.appendChild(cidade);
     }
 }
 
 gerar(listaIdaEvolta)
 gerar(listaIda)
-
-
-function renderPage() {
-    console.log('click');
-}
-
